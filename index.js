f13a79f857cf06cddfbbf21ee5394c30d3405056
Nicolas Pupo <nicolaspwpo@gmail.com>
2024-09-27 20:01:17 -0300

    Atualização do código

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-xp = (2000)
+let xp = (10001)
+let rank = ""
+let nome = "Nicolas"
 
-if (xp < 1000) {
-    console.log("Ferro");
+if (xp <= 1000) {
+    rank = "Ferro"
 } else if (xp >= 1001 && xp <= 2000) {
-    console.log("Bronze");
+    rank = "Bronze"
 } else if (xp >= 2001 && xp <= 5000) {
-    console.log("Prata");
+    rank = "Prata"
 } else if (xp >= 5001 && xp <= 7000) {
-    console.log("Ouro");
+    rank = "Ouro"
 } else if (xp >= 7001 && xp <= 8000) {
-    console.log("Platina");
+    rank = "Platina"
 } else if (xp >= 8001 && xp <= 9000) {
-    console.log("Ascendente");
+    rank = "Ascendente"
 } else if (xp >= 9001 && xp <= 10000) {
-    console.log("Imortal");
+    rank = "Imortal"
 } else {
-    console.log("Radiante");
+    rank = "Radiante"
 }
+
+console.log("O Herói de nome " + nome + " está no nível " + rank)
